a49e0b3d720d2481e6aadb19f75dbbb3ae79f954
FlutLab.io <>
2021-11-24 20:17:30 +0000

    Autosave

--- lib/pages/addstudent.dart
+++ lib/pages/addstudent.dart
@@ -8,18 +8,20 @@ class addstudentpage extends StatefulWidget {
 class _addstudentpageState extends State<addstudentpage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          child: Text(
-            'Back to home',
-            style: TextStyle(fontSize: 20.0),
+    return Container(
+      child: Column(
+        children: [
+          ElevatedButton(
+            child: Text(
+              'Back to home',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => myHomepage()));
+            },
           ),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => myHomepage()));
-          },
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
