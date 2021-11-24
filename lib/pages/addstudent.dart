e75f3ce80bbaad0191b0fa5dbb57c771edeb32d9
FlutLab.io <>
2021-11-24 19:14:16 +0000

    Autosave

--- lib/pages/addstudent.dart
+++ lib/pages/addstudent.dart
@@ -8,7 +8,10 @@ class _addstudentpageState extends State<addstudentpage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('awais'),
+      child: Text(
+        'awais',
+        style: TextStyle(color: Colors.accents),
+      ),
     );
   }
 }
