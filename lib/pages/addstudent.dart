730a0add165671febc842a63ef231dccc98fa40c
FlutLab.io <>
2021-11-24 19:27:48 +0000

    Autosave

--- lib/pages/addstudent.dart
+++ lib/pages/addstudent.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 class addstudentpage extends StatefulWidget {
   _addstudentpageState createState() => _addstudentpageState();
@@ -8,7 +9,19 @@ class _addstudentpageState extends State<addstudentpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text('Awais')),
+      body: Column(
+        children: [
+          ElevatedButton(
+            child: Text(
+              'Add',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => myHomepage()));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
